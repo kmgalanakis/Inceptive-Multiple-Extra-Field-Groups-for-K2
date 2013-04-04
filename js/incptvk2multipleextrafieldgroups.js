edf647d2c25b82ba76e2290a858e5b433c12da6b
kmgalanakis <kmgalanakis@gmail.com>
2013-04-04 17:26:43 +0300

    version change

--- js/incptvk2multipleextrafieldgroups.js
+++ js/incptvk2multipleextrafieldgroups.js
@@ -1,5 +1,5 @@
 /**
- * @version		1.0
+ * @version		1.1
  * @package		Inceptive Mutliple Extra Field Groups for K2
  * @author		Inceptive Design Labs - http://www.inceptive.gr
  * @copyright	Copyright (c) 2006 - 2012 Inceptive GP. All rights reserved.
--- plugins/system/incptvk2multipleextrafieldgroups.php
+++ plugins/system/incptvk2multipleextrafieldgroups.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version		1.0
+ * @version		1.1
  * @package		Inceptive Mutliple Extra Field Groups for K2
  * @author		Inceptive Design Labs - http://www.inceptive.gr
  * @copyright	Copyright (c) 2006 - 2012 Inceptive GP. All rights reserved.
